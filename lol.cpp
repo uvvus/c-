588505ee90dad2588fcb53a9d4e8700b5416118f
uvvus <113056688+uvvus@users.noreply.github.com>
2024-01-08 15:28:34 +0100

    Update lol.cpp

--- lol.cpp
+++ lol.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdlib.h>
+#include <fstream>
 
 
 using namespace std;
@@ -11,7 +12,18 @@ void Dodaj(int *n, int *p)
 
 void Wstaw(int *n, int *p)
 {
-
+    int x;
+    do
+    {
+        cout << "Numer indeksu gdzie chcesz wstawić element: ";
+        cin >> x;
+    }
+    while(x >= *n);
+    {
+        int w;
+        cin >> w;
+        p[x] = w;
+    }
 }
 
 void Usun(int *n, int *p)
@@ -20,6 +32,21 @@ void Usun(int *n, int *p)
 }
 
 void Wyswietl(int *n, int *p)
+{
+    cout << "Tablica: [";
+    for(int i = 0; i > *n; i++)
+    {
+        cout << p[i] << ",";
+    }
+    cout << "]" << endl;
+}
+
+void Zapisz(int *n, int *p)
+{
+
+}
+
+void Wczytaj(int *n, int *p)
 {
 
 }
@@ -35,7 +62,9 @@ int menu()
     cout << " [2] Wstaw element" << endl;
     cout << " [3] Usun element" << endl;
     cout << " [4] Wyswietl tablice" << endl;
-    cout << " [5] Wyjdz z programu" << endl;
+    cout << " [5] Zapisz plik" << endl;
+    cout << " [6] Wczytywanie pliku" << endl;
+    cout << " [0] Wyjdz z programu" << endl;
     cout << "-------------------------" << endl;
     cout << "Wybierz opcje: ";
 
@@ -48,31 +77,53 @@ int menu()
 int main()
 {
     int *p = NULL;
+    int n = 0;
     bool x = true;
+
     while(x == true)
         switch(menu())
         {
         case 1:
-            cout << "test" << endl;
-            break;
+            {
+                int * tmp = NULL;
+                tmp = new int[n+1];
+                if(p != NULL)
+                {
+                    for(int i=0;i<n;i++)
+                    {
+                        tmp[i] = p[i];
+                    }
+
+                    delete[] p;
+
+                }
+                cout << "Podaj wartosc elementu tablicy: ";
+                cin >> tmp[n];
+                p = tmp;
+                n++;
+                break;
+            }
         case 2:
-            cout << "test" << endl;
+            cout << "test2" << endl;
             break;
         case 3:
-            cout << "test" << endl;
+            cout << "test3" << endl;
             break;
         case 4:
-            cout << "test" << endl;
+            cout << "test4" << endl;
             break;
         case 5:
+            cout << "test5" << endl;
+            break;
+        case 6:
+            cout << "test6" << endl;
+            break;
+        case 0:
             x = false;
             break;
         default:
             cout << "Wprowadz poprawna liczbe!" << endl;
     }
-
-
-
 
     return 0;
 }
